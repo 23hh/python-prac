0c0c56d54c5d6518e10af56ddca5f3381f6854b6
Hyeop <lsh6166@gmail.com>
2025-06-18 17:16:05 +0900

    0618-2

---
+++ coding-test/20250618/multiple_N.py
@@ -0,0 +1,9 @@
+'''
+https://school.programmers.co.kr/learn/courses/30/lessons/181937
+
+num % n == 0 で倍数かどうかを判定し、
+条件に応じて 1 または 0 を返す。
+'''
+
+def solution(num, n):
+    return 1 if num % n == 0 else 0
