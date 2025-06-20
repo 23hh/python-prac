22670a4170dbdb0aa63ccf401dca2f7c6cf0efe4
Hyeop <lsh6166@gmail.com>
2025-06-20 17:50:18 +0900

    오타수정

--- coding-test/20250620/height.js
+++ coding-test/20250620/height.js
@@ -1,6 +1,6 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120585
 
-// 配列を捜査して、heightより大きい要素だけを抽出し、
+// 配列を操作して、heightより大きい要素だけを抽出し、
 // その個数を数える。
 
 function solution(array, height) {
