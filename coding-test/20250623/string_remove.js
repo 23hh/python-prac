da695c6f1a1656aab5595d7ac57ca11acfcd58b9
Hyeop <lsh6166@gmail.com>
2025-06-23 17:27:31 +0900

    0623-1

--- coding-test/20250623/string_remove.js
+++ coding-test/20250623/string_remove.js
@@ -1,6 +1,6 @@
-//school.programmers.co.kr/learn/courses/30/lessons/120826
+// //school.programmers.co.kr/learn/courses/30/lessons/120826
 
-https: 特定文字列を除いてjoinを使って結合する;
+// https: 特定文字列を除いてjoinを使って結合する;
 
 function solution(my_string, letter) {
   return my_string.split(letter).join("");
